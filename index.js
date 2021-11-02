de43764398830226131ba2beb89415f7bc1f1682
denysvedernikov02 <32486072+denysvedernikov02@users.noreply.github.com>
2021-11-02 14:54:01 +0200

    Hiding login and password of DB

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use('/api/auth', require('./routes/auth.route'))
 
 async function start() {
     try{
-        await mongoose.connect('mongodb+srv://denys_admin:dtlthysrjd12@cluster0.ytxdy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+        await mongoose.connect('mongodb+srv://login:password@cluster0.ytxdy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
             useNewUrlParser: true,
             useUnifiedTopology: true
         })
